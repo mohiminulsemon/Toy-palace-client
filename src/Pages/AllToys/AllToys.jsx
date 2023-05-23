f31ab0d24fd5db0eace6a7771af4821c0345eee2
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-23 15:34:54 +0600

    updated search options in all toys

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,66 +1,90 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
-
-
 const AllToys = () => {
+  const toys = useLoaderData();
+  // console.log(Toys);
 
-    const toys = useLoaderData();
-    // console.log(Toys);
-    return (
-        <div>
-        <h1>All Toys</h1>
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Seller
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Toy Name
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Sub-category
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Price
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Available Quantity
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredToys = toys.filter((toy) =>
+    toy.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div>
+      <h1 className="text-center font-bold">All Toys</h1>
+      <div className="my-4">
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search by Toy Name"
+          className="border border-gray-300 rounded-md px-4 py-2 w-64"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Seller
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Toy Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Sub-category
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Price
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Available Quantity
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Image
             </th>
-              <th className="px-6 py-3"></th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {toys.map((toy) => (
-              <tr key={toy._id}>
-                <td className="px-6 py-4 whitespace-nowrap">{toy.sellerName}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{toy.name}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{toy.subCategory}</td>
-                <td className="px-6 py-4 whitespace-nowrap">${toy.price}</td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  {toy.availableQuantity}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
+            <th className="px-6 py-3"></th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {filteredToys.map((toy) => (
+            <tr key={toy._id}>
+              <td className="px-6 py-4 whitespace-nowrap">{toy.sellerName}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{toy.name}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{toy.subCategory}</td>
+              <td className="px-6 py-4 whitespace-nowrap">${toy.price}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {toy.availableQuantity}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
                 <img
                   src={toy.pictureUrl}
                   alt={toy.name}
                   className="h-20 w-20"
                 />
               </td>
-                <td className="px-6 py-4 whitespace-nowrap">
+              <td className="px-6 py-4 whitespace-nowrap">
                 <Link to={`/alltoys/${toy._id}`}>
-            <button className="btn btn-secondary text-white"> View Details</button>
-          </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
+                  <button className="btn btn-secondary text-white">
+                    {" "}
+                    View Details
+                  </button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AllToys;
